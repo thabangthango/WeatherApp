c7bd9d76f7b3a7d388a1e67cae21fb74563130c0
Thabang Thango <thabang@glucode.com>
2021-07-07 17:20:28 +0200

    Fix folder structure

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Thabang Thango on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
